6dffafc535988f4053fee802e78f5146fa261cf2
limwoojae <128343537+limwoojae@users.noreply.github.com>
2023-03-20 16:44:46 +0900

    commit

---
+++ drive/src/main/java/google/drive/domain/FileUploaded.java
@@ -0,0 +1,24 @@
+package google.drive.domain;
+
+import google.drive.domain.*;
+import google.drive.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class FileUploaded extends AbstractEvent {
+
+    private Long id;
+    private String name;
+    private String path;
+    private String type;
+
+    public FileUploaded(File aggregate) {
+        super(aggregate);
+    }
+
+    public FileUploaded() {
+        super();
+    }
+}
